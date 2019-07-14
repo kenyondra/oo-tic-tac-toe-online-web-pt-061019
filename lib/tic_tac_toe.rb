b3ccd7b6ef4d3515919eff813720f48ad100e6e0
Kenyondra J Conley <kenyondra_conley@yahoo.com>
2019-07-14 05:06:58 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -101,7 +101,8 @@ class TicTacToe
       WIN_COMBINATIONS.detect do |combo|
         if @board[combo[0]] == "X" && @board[combo[1]] == "X" && @board[combo[2]] == "X"
           return "X"
-          elsif @board[combo[0]] == "O" && @board[combo[1]] == "O" && @board[combo[2]] 
+          elsif @board[combo[0]] == "O" && @board[combo[1]] == "O" && @board[combo[2]] == "O"
+          
       
       
       
