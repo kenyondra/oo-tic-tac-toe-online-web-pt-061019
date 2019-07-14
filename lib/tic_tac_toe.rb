fcbbc8c26f72b4fd0d83df74c62f90df4b7408e2
Kenyondra J Conley <kenyondra_conley@yahoo.com>
2019-07-14 05:05:23 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -100,6 +100,7 @@ class TicTacToe
     def winner
       WIN_COMBINATIONS.detect do |combo|
         if @board[combo[0]] == "X" && @board[combo[1]] == "X" && @board[combo[2]] == "X"
+          return "X"
           
       
       
